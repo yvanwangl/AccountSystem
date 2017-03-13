bcfdfad03d1e1eecda432356047608ecec4f8794
wangyafei <wangyafei@startimes.com.cn>
2017-03-13 22:15:26 +0800

    modify settlement

---
+++ src/components/Settlement/SettlementList/SettlementList.jsx
@@ -0,0 +1,120 @@
+import React, {Component, PropTypes} from 'react';
+import {Table, Pagination, Popconfirm, Button} from 'antd';
+import dateFormat from '../../../utils/dateFormat';
+import {PAGE_SIZE} from '../../../constants/constants';
+import Spliter from '../../Spliter/Spliter';
+import * as moment from 'moment';
+import {settlementList} from './index.css';
+
+const SettlementList = ({
+    total,
+    current,
+    loading,
+    dataSource,
+    onPageChange,
+    onModify,
+    onReadOnly,
+    onDel
+}) => {
+    const columns = [
+        {
+            title: '序号',
+            dataIndex: 'serialNumber',
+            key: 'serialNumber',
+            render: (text, record, index)=><span>{index + 1}</span>
+        },
+        {
+            title: '进货日期',
+            dataIndex: 'createInstance',
+            key: 'createInstance',
+            render: (text)=><span>{moment.parseZone(text).local().format('YYYY-MM-DD HH:mm')}</span>
+        },
+        {
+            title: '单据编号',
+            dataIndex: 'noteNumber',
+            key: 'noteNumber'
+        },
+        {
+            title: '供应商名称',
+            dataIndex: 'supplierName',
+            key: 'supplierName'
+        },
+        {
+            title: '应付金额',
+            dataIndex: 'totalAmount',
+            key: 'totalAmount',
+        },
+        {
+            title: '已付金额',
+            dataIndex: 'paymentAmount',
+            key: 'paymentAmount',
+        },
+		{
+			title: '备注',
+			dataIndex: 'mem',
+			key: 'mem',
+		},
+        {
+            title: '操作',
+            key: 'operation',
+            render: (text, record)=>(
+                <p>
+                    <a onClick={()=> onModify(record['_id'])}>编辑</a>
+                    <Spliter spliterText="|"/>
+                    <Popconfirm title="确定删除该条记录？" onConfirm={()=> onDel(record['_id'])}>
+                        <a type='ghost'>删除</a>
+                    </Popconfirm>
+                    <Spliter spliterText="|"/>
+                    <a onClick={()=> onReadOnly(record['_id'])}>详情</a>
+                </p>
+            )
+        }
+    ];
+
+    const rowSelection = {
+        onChange: (selectedRowKeys, selectedRows) => {
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+        },
+        onSelect: (record, selected, selectedRows) => {
+            console.log(record, selected, selectedRows);
+        },
+        onSelectAll: (selected, selectedRows, changeRows) => {
+            console.log(selected, selectedRows, changeRows);
+        },
+        getCheckboxProps: record => ({
+            disabled: record.name === 'Disabled User',    // Column configuration not to be checked
+        }),
+    };
+
+    return (
+        <div className={settlementList}>
+            <Table
+                columns={columns}
+                dataSource={dataSource}
+                loading={loading}
+                rowKey={record=>record._id}
+                pagination={false}
+                rowSelection={rowSelection}
+            />
+            <Pagination
+                className="ant-table-pagination"
+                total={total}
+                current={parseInt(current)}
+                pageSize={PAGE_SIZE}
+                onChange={onPageChange}
+            />
+        </div>
+    );
+};
+
+SettlementList.propTypes = {
+    onPageChange: PropTypes.func,
+    onModify: PropTypes.func,
+    onDel: PropTypes.func,
+    dataSource: PropTypes.array,
+    loading: PropTypes.any,
+    total: PropTypes.any,
+    current: PropTypes.any
+};
+
+export default SettlementList;
---
+++ src/components/Settlement/SettlementSearchForm/SettlementSearchForm.jsx
@@ -0,0 +1,73 @@
+import React, {Component, PropTypes} from 'react';
+import {Form, Input, Select, Button, DatePicker} from 'antd';
+import {settlementSearchForm} from './index.css';
+
+const FormItem = Form.Item;
+const RangePicker = DatePicker.RangePicker;
+const Option = Select.Option;
+
+const SettlementSearchForm = ({
+    onSearch,
+	suppliers,
+    form: {
+        getFieldDecorator,
+        getFieldsValue,
+        validateFields
+    }
+}) => {
+    const onSubmit = (e)=> {
+        e.preventDefault();
+        validateFields((errors, values)=> {
+            if (!!errors) {
+                return false;
+            }
+            if(values['timeRange']){
+            	values['timeRange'] = values['timeRange'].map((time)=> time.toLocaleString());
+			}
+            onSearch(values);
+        })
+    };
+
+    return (
+        <div className={settlementSearchForm}>
+            <Form inline onSubmit={onSubmit}>
+                <FormItem>
+                    {
+                        getFieldDecorator('timeRange')(
+                            <RangePicker size='large'/>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="供应商名称：">
+                    {
+                        getFieldDecorator('supplierId')(
+                            <Select style={{minWidth: 150}}>
+								{
+									suppliers.map(({_id, supplierName})=>(
+										<Option key={_id}>{supplierName}</Option>
+									))
+								}
+                            </Select>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="订单编号：">
+                    {
+                        getFieldDecorator('noteNumber')(
+                            <Input type="text"/>
+                        )
+                    }
+                </FormItem>
+                <Button type='primary' htmlType='submit'>搜索</Button>
+            </Form>
+        </div>
+    );
+};
+
+SettlementSearchForm.propTypes = {
+    form: PropTypes.object,
+    onSearch: PropTypes.func,
+	customers: PropTypes.array
+};
+
+export default Form.create()(SettlementSearchForm);
