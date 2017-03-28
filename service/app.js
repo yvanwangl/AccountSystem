95e5cce9a7897eacee46b4dae8fcfef1b4d63ac4
wangyafei <wangyafei@startimes.com.cn>
2017-03-28 14:59:53 +0800

    modify upload files

--- service/app.js
+++ service/app.js
@@ -28,7 +28,7 @@ let app = express();
 
 app.use(compression());
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use(express.static(path.join(__dirname, '../uploadfiles')));
+app.use(express.static(path.join(__dirname, '../public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
